af791b3ed36b51aa7fe273f4a11c32c6e1bb40d6
Keenan McDonald <keemcd@gmail.com>
2020-09-20 19:04:43 -0500

    transform, slideInLeft and scrollOut working

--- index.js
+++ index.js
@@ -17,14 +17,124 @@ const projects = [
     }
 ]
 
-function handleScroll(){
-    const windowTop = $(window).scrollTop()
-
-    for (project of projects){
-        scrollInProject(project.id, windowTop)
+function sizeBlocks(){
+    for (const elem of document.querySelectorAll("[class*=block]")){
+        const classList = elem.className.split(' ')
+        for (const className of classList){
+            if (className.startsWith('block-')){
+                const height = parseInt(className.slice(6))
+                if (Number.isInteger(height)){
+                    elem.style.height = height + 'vh'
+                    elem.style.position = 'relative'
+                    elem.style.top = 0
+                }
+            }
+        }
     }
 }
 
+//TODO: start / end on percentage and px needs to function like vh
+//blockSelector rather than blockId
+function transform(blockId, targetSelector, callback, start=0, end=100, unit='%'){
+
+    const windowTop = window.pageYOffset
+    const blockTop = document.getElementById(blockId).offsetTop
+    const blockHeight = document.getElementById(blockId).offsetHeight
+
+    let progress
+    if (unit === '%'){
+        absoluteProgress = (windowTop - blockTop)*100 / (blockHeight-window.innerHeight)
+        if (absoluteProgress < start){
+            progress = 0
+        }
+        else if (absoluteProgress >= start && absoluteProgress <= end){
+            progress = 100 * (absoluteProgress - start) / (end-start)
+        }
+        else{
+            progress = 100
+        }
+    }
+    else if (unit === 'vh'){
+        absoluteProgress = ((windowTop-blockTop)*100 / window.innerHeight)
+
+        if (absoluteProgress < start){
+            progress = 0
+        }
+        else if (absoluteProgress >= start && absoluteProgress <= end){
+            progress = absoluteProgress - start
+        }
+        else{
+            progress = end - start
+        }
+    
+    }
+    else if (unit === 'px'){
+        //need to handle start/end
+        absoluteProgress = (windowTop-blockTop)
+        progress = absoluteProgress
+    }
+    else{
+        throw Error(`unit must be '%', 'vh', or 'px'`)
+    }
+
+
+
+    for (target of document.getElementById(blockId).querySelectorAll(targetSelector)){
+        //console.log(target)
+        return callback(target, progress, start, end)
+    }
+}
+
+function slideInLeft(blockId, targetSelector, start=0, end=100, unit='%'){
+    //TODO: progress needs to reflect start / end
+    transform(blockId, targetSelector, (target, progress, start, end) => {
+        target.style.position = 'fixed'
+        target.style.marginLeft = `${100-progress}vw`
+    },
+        start, end, unit)
+}
+
+function scrollOut(blockId, targetSelector){
+    const end = document.getElementById(blockId).offsetHeight*100 / window.innerHeight
+    const start = end - 100
+
+    transform(blockId, targetSelector, (target, progress) => {
+        target.style.position = 'fixed'
+        target.style.marginTop = `-${progress}vh`
+    },
+    start, end, 'vh')
+}
+//function slideOutLeft(block, target, start=0, end=100, unit='%')
+//function slideInRight(block, target, start=0, end=100, unit='%')
+//function slideOutRight(block, target, start=0, end=100, unit='%')
+//function slideInTop(block, target, start=0, end=100, unit='%')
+//function slideOutTop(block, target, start=0, end=100, unit='%')
+//function slideInBottom(block, target, start=0, end=100, unit='%')
+//function slideOutBottom(block, target, start=0, end=100, unit='%')
+//function fadeIn(block, target, startOpacity, endOpacity, start=0, end=100, unit='%')
+//function fadeOut(block, target, startOpacity, endOpacity, start=0, end=100, unit='%')
+//function expand(block, target, startSize, endSize, start=0, end=100, unit='%')
+//function contract(block, target, startSize, endSize, start=0, end=100, unit='%')
+
+
+function test(){
+    const blockIds = ['lostart', 'terra', 'whynotme']
+    for (blockId of blockIds){
+        slideInLeft(blockId, '.text-container', 0, 50)
+        scrollOut(blockId, '.text-container')
+    }
+
+    //transform('lostart', '.text-container', undefined, undefined, undefined, '%')
+    //transform('lostart', '.text-container', undefined, undefined, undefined, 'vh')
+    //transform('lostart', '.text-container', undefined, undefined, undefined, 'px')
+}
+
+function handleScroll(){
+    const windowTop = $(window).scrollTop()
+
+    test()
+}
+/*
 function scrollInProject(id, windowTop){
     const progress = 3*(windowTop - $(`#${id}`).position().top) / $(`#${id}`).height()
     console.log(id, progress)
@@ -46,13 +156,14 @@ function scrollInProject(id, windowTop){
     else{
         //$(`#${id} .text-container`).css('top', 0)
     }
-}
+}*/
 
 function handleListeners(){
     window.addEventListener('scroll', handleScroll);}
 
 function handlePortfolio(){
     handleListeners()
+    sizeBlocks()
 }
 
 $(handlePortfolio)
